d0feedd9de4710e8a48116709db53595290de395
Cody Hilborn <codyhilborn11@gmail.com>
2023-02-22 19:58:16 -0500

    Add Empty component to Appointment, text w/ storybook

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem.jsx";
 import InterviewerList from "components/InterviewerList.jsx";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.jsx";
+import Empty from "components/Appointment/Empty.jsx";
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -169,6 +170,7 @@ storiesOf('Appointment', module)
   ))
   // *** Header Testing ***
   .add('Header', () => <Header time={'12pm'} />)
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />);
 
 
 
