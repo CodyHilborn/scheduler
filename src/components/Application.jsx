5aee5b8b7a29a3253416166736b572f437448d76
Cody Hilborn <codyhilborn11@gmail.com>
2023-02-23 19:13:54 -0500

    Move old state declarations into new centralized state object

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -48,14 +48,21 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState(prev => ({ ...prev, days }));
+
 
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
+    axios.get('/api/days')
       .then(response => setDays(response.data));
   }, []);
-  
+
 
   const appointmentArr = Object.values(appointments).map((appointment) => {
     return <Appointment
@@ -75,8 +82,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
